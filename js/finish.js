6dbfd5b7044a7517d3b6c2e3b0f39ddb611dd19d
Achraf <chibane.tkd04@gmail.com>
2022-12-05 20:11:00 -0700

    updated the finish.js file

--- js/finish.js
+++ js/finish.js
@@ -28,5 +28,5 @@ saveHighScore = (e) => {
 
 //  local Storage 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('./');
+    window.location.assign('./index.html');
 };
